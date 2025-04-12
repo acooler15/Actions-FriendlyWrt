5f83bbe56e535d8f937181199c60ea1c551ec3c7
acooler15 <renzongjia@vip.qq.com>
2025-04-12 11:34:06 +0800

    移除docker

--- scripts/custome_config.sh
+++ scripts/custome_config.sh
@@ -3,3 +3,6 @@
 sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
 sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
+sed -i -e 's/CONFIG_PACKAGE_dockerd=y/# CONFIG_PACKAGE_dockerd is not set/g' configs/rockchip-docker/06-docker
+sed -i -e 's/CONFIG_PACKAGE_docker-compose=y/# CONFIG_PACKAGE_docker-compose is not set/g' configs/rockchip-docker/06-docker
+sed -i -e 's/CONFIG_PACKAGE_luci-app-dockerman=y/# CONFIG_PACKAGE_luci-app-dockerman is not set/g' configs/rockchip-docker/06-docker
